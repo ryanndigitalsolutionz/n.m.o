b7ea406ce3f2477b1682f6211894676d9b0e13d7
Ryan Makori <ryanndigitalsolutionz@gmail.com>
2026-08-08 15:30:10 +0300

    Clean up staff_resource.py by removing unused code
    Removed unused imports and app context print statement.

--- raw-api/resources/staff_resource.py
+++ raw-api/resources/staff_resource.py
@@ -9,11 +9,6 @@ from flask_jwt_extended import (
     get_jwt,
     get_jwt_identity,
 )
-from app import app
-from extensions import db
-
-with app.app_context():
-    print(db.engine.url)
 
 class StaffListResource(Resource):
 
